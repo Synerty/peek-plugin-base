60c039b6c421f05a1ff8dbc6bbda6040f0831005
Jarrod Chesney <jarrod.chesney@synerty.com>
2017-03-17 18:43:06 +1000

    Removed docs folder, added comments to vortex name

--- peek_plugin_base/PeekVortexUtil.py
+++ peek_plugin_base/PeekVortexUtil.py
@@ -1,4 +1,16 @@
+
+
+#: The vortex name for the Server service
 peekServerName = "peek-server"
+
+#: The vortex name for the Client service
 peekClientName = "peek-client"
+
+#: The vortex name for the Agent service
 peekAgentName = "peek-agent"
+
+#: The vortex name for the Worker service
 peekWorkerName = "peek-worker"
+
+#: This is a cool test
+aNarlyVar = "AWESOME"
