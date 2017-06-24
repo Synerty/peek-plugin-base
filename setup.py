0f182e617f5969706d4930b8d614075f02af679a
Jarrod Chesney <jarrod.chesney@synerty.com>
2017-06-24 22:19:29 +1000

    version bump vortexpy and txhttputil

--- setup.py
+++ setup.py
@@ -35,8 +35,8 @@ requirements = [
     
     # Protocol and data packages
     "pytmpdir >= 0.2.3",  # A temporary directory, useful for extracting archives to
-    "txhttputil >= 0.1.12",  # Utility class for http requests
-    "vortexpy >= 0.7.7",  # Data serialisation and transport layer, observable based
+    "txhttputil >= 0.2.0",  # Utility class for http requests
+    "vortexpy >= 0.7.8",  # Data serialisation and transport layer, observable based
 
     # SOAP interface packages
     "SOAPpy-py3 >= 0.52.24",  # See http://soappy.ooz.ie for tutorials
