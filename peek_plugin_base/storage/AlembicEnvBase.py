4ef0c8d441f96343ea9fdb8f81645dd3142ba5ab
Jarrod Chesney <jarrod.chesney@synerty.com>
2020-05-24 15:33:44 +1000

    Revert "PEEK-612: imp: Added support for timescale in plugins"
    This reverts commit dd59ecd3

--- peek_plugin_base/storage/AlembicEnvBase.py
+++ peek_plugin_base/storage/AlembicEnvBase.py
@@ -3,14 +3,11 @@ from sqlalchemy import engine_from_config, pool
 from sqlalchemy.dialects.mssql.base import MSDialect
 from sqlalchemy.dialects.postgresql.base import PGDialect
 
-
 def isMssqlDialect(engine):
     return isinstance(engine.dialect, MSDialect)
 
-
 def isPostGreSQLDialect(engine):
     return isinstance(engine.dialect, PGDialect)
-
 
 def ensureSchemaExists(engine, schemaName):
     # Ensure the schema exists
@@ -26,7 +23,6 @@ def ensureSchemaExists(engine, schemaName):
     else:
         raise Exception('unknown dialect %s' % engine.dialect)
 
-
 class AlembicEnvBase:
     def __init__(self, targetMetadata):
         from peek_platform.util.LogUtil import setupPeekLogger
@@ -36,12 +32,19 @@ class AlembicEnvBase:
         self._targetMetadata = targetMetadata
         self._schemaName = targetMetadata.schema
 
+    def _includeObjectFilter(self, object, name, type_, reflected, compare_to):
+        # If it's not in this schema, don't include it
+        if hasattr(object, 'schema') and object.schema != self._schemaName:
+            return False
+
+        return True
+
     def run(self):
         """Run migrations in 'online' mode.
-
+    
         In this scenario we need to create an Engine
         and associate a connection with the context.
-
+    
         """
         connectable = engine_from_config(
             self._config.get_section(self._config.config_ini_section),
@@ -54,7 +57,8 @@ class AlembicEnvBase:
             context.configure(
                 connection=connection,
                 target_metadata=self._targetMetadata,
-                include_schemas=False,
+                include_object=self._includeObjectFilter,
+                include_schemas=True,
                 version_table_schema=self._schemaName
             )
 
