93081048d09d6b91b6bc77d7dc465a0154d22b6b
Jarrod Chesney <jarrod.chesney@synerty.com>
2020-06-03 19:15:32 +1000

    PEEK-627: imp: Upgraded to "vortexpy >= 1.3.1"

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ requirements = [
     # Protocol and data packages
     "pytmpdir >= 0.2.3",  # A temporary directory, useful for extracting archives to
     "txhttputil >= 0.3.5",  # Utility class for http requests
-    "vortexpy >= 1.3.0",  # Data serialisation and transport layer, observable based
+    "vortexpy >= 1.3.1",  # Data serialisation and transport layer, observable based
 
     # SOAP interface packages
     "SOAPpy-py3 >= 0.52.24",  # See http://soappy.ooz.ie for tutorials
