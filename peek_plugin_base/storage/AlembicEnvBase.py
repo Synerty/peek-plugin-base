a17f13958d141277b030d3cfd9e0220bd1ca996d
Louis Lu <louis.lu@synerty.com>
2020-12-03 22:20:54 +0000

    PEEK-692 fix alembic revision autogenerate
    "alembic revision --autogenerate" raises
    sqlalchemy.exc.NoSuchTableError.
    
    The cause is that when alembic runs the mirgration, it fails to
    filter other schmeas even if there is a includeFilter specified.
    
    The fix is to configure alembic to compare the changes with each
    schema and make transcations for each schema instead of doing a
    transcation for all changes.

--- peek_plugin_base/storage/AlembicEnvBase.py
+++ peek_plugin_base/storage/AlembicEnvBase.py
@@ -1,66 +1,79 @@
+import logging
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 from sqlalchemy.dialects.mssql.base import MSDialect
 from sqlalchemy.dialects.postgresql.base import PGDialect
 
+log = logging.getLogger(__name__)
+
+
 def isMssqlDialect(engine):
     return isinstance(engine.dialect, MSDialect)
+
 
 def isPostGreSQLDialect(engine):
     return isinstance(engine.dialect, PGDialect)
 
+
 def ensureSchemaExists(engine, schemaName):
     # Ensure the schema exists
-
     if isinstance(engine.dialect, MSDialect):
         if list(engine.execute("SELECT SCHEMA_ID('%s')" % schemaName))[0][0] is None:
             engine.execute("CREATE SCHEMA [%s]" % schemaName)
-
     elif isinstance(engine.dialect, PGDialect):
-        engine.execute(
-            'CREATE SCHEMA IF NOT EXISTS "%s" ' % schemaName)
-
+        engine.execute('CREATE SCHEMA IF NOT EXISTS "%s" ' % schemaName)
     else:
-        raise Exception('unknown dialect %s' % engine.dialect)
+        raise Exception("unknown dialect %s" % engine.dialect)
+
 
 class AlembicEnvBase:
     def __init__(self, targetMetadata):
         from peek_platform.util.LogUtil import setupPeekLogger
-        setupPeekLogger()
 
+        setupPeekLogger()
         self._config = context.config
         self._targetMetadata = targetMetadata
         self._schemaName = targetMetadata.schema
 
     def _includeObjectFilter(self, object, name, type_, reflected, compare_to):
         # If it's not in this schema, don't include it
-        if hasattr(object, 'schema') and object.schema != self._schemaName:
+        if hasattr(object, "schema") and object.schema != self._schemaName:
             return False
-
         return True
 
     def run(self):
         """Run migrations in 'online' mode.
-    
         In this scenario we need to create an Engine
         and associate a connection with the context.
-    
         """
         connectable = engine_from_config(
             self._config.get_section(self._config.config_ini_section),
-            prefix='sqlalchemy.',
-            poolclass=pool.NullPool)
-
+            prefix="sqlalchemy.",
+            poolclass=pool.NullPool,
+        )
         with connectable.connect() as connection:
             ensureSchemaExists(connectable, self._schemaName)
-
-            context.configure(
-                connection=connection,
-                target_metadata=self._targetMetadata,
-                include_object=self._includeObjectFilter,
-                include_schemas=True,
-                version_table_schema=self._schemaName
-            )
-
-            with context.begin_transaction():
-                context.run_migrations()
+            peekSchemas = [
+                row[0]
+                for row in connection.execute(
+                    """SELECT SCHEMA_NAME FROM
+                      INFORMATION_SCHEMA.SCHEMATA
+                      WHERE SCHEMA_OWNER = 'peek';"""
+                )
+            ]
+            for peekSchema in peekSchemas:
+                log.info("Migrating Peek schema %s" % peekSchema)
+                schemaWiseConnection = connection.execution_options(
+                    schema_translate_map={None: peekSchema}
+                )
+                context.configure(
+                    connection=schemaWiseConnection,
+                    target_metadata=self._targetMetadata,
+                    include_object=self._includeObjectFilter,
+                    include_schemas=False,
+                    # process_revision_directives=self._process_revision_directives,
+                    version_table_schema=self._schemaName,
+                )
+                # schema-wise transactions
+                with context.begin_transaction():
+                    context.run_migrations()
