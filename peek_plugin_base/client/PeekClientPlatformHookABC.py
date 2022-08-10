6fc3301c2cbca4c6c5ee431a2787a3735d508f25
Louis Lu <louis.lu@synerty.com>
2022-08-10 16:12:45 +1200

    PEEK-1631 rebased from project/master
    # Conflicts:
    #	peek_plugin_base/client/PluginClientEntryHookABC.py

--- peek_plugin_base/client/PeekClientPlatformHookABC.py
+++ peek_plugin_base/client/PeekClientPlatformHookABC.py
@@ -2,7 +2,9 @@ from peek_plugin_base.PeekPlatformCommonHookABC import PeekPlatformCommonHookABC
 from peek_plugin_base.PeekPlatformFileStorageHookABC import (
     PeekPlatformFileStorageHookABC,
 )
-from peek_plugin_base.PeekPlatformServerInfoHookABC import PeekPlatformServerInfoHookABC
+from peek_plugin_base.PeekPlatformServerInfoHookABC import (
+    PeekPlatformServerInfoHookABC,
+)
 from peek_plugin_base.client.PeekPlatformOfficeHttpHookABC import (
     PeekPlatformOfficeHttpHookABC,
 )
